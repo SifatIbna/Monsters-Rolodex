c2fadf67d89d8f6e599414609a965fa804447487
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-05-05 21:58:52 +0600

    initial commit

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => this.setState({ monsters: user }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList name="Sifat">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
